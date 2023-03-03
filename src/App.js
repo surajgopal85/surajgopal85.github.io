d101bfa40692f9fa7f9b8b6b35361e16196077c5
Suraj Gopal <surajcancode@gmail.com>
2023-03-03 18:12:10 -0500

    added style, navbar logic --- open/close hamburger, icon appears relative to page width

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
+// UI imports
+import Navbar from './UI/Navbar';
 // style import
 import './App.css';
+import './styles/Navbar.css';
 
 function App() {
   return (
     <div className="App">
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/projects' element={<Projects />}/>
