e577c867112c52f46d6ffa217bdb5e39a2eb1c6d
Suraj Gopal <surajcancode@gmail.com>
2023-03-03 15:19:50 -0500

    main 3 pages created as functional components, add routing logic to app.js, import pages to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+// router imports
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// pages imports
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
+// style import
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/projects' element={<Projects />}/>
+        <Route path='/experience' element={<Experience />}/>
+      </Routes>
+    </Router>
     </div>
   );
 }
