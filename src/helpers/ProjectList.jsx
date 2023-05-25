23e00b8a8b5ab272a72758aa669f68bd307c3eb0
Suraj Gopal <surajcancode@gmail.com>
2023-05-25 14:04:06 -0400

    project list update - spring boot library

--- src/helpers/ProjectList.jsx
+++ src/helpers/ProjectList.jsx
@@ -1,18 +1,21 @@
 import Local from '../assets/Local-landing.png';
 // import test from '../assets/test.jpeg';
 import coming_soon from '../assets/coming_soon.jpeg'
+import spring_boot_library from '../assets/spring_boot_library.png'
 
 export const ProjectList = [
   {
     name: 'Local',
     image: Local,
     skills: 'Ruby on Rails, PostgreSQL, HTML, CSS, Bootstrap, Simple Form, Mapbox API, Flatpicker',
-    link: 'http://www.local-1038.xyz/'
+    link: 'http://www.local-1038.xyz/',
+    description: 'A travel app where users get suggestions for trip experiences filtered by their preferences & located in the city of their choice.'
   },
   {
-    name: 'Test',
-    image: coming_soon,
-    skills: 'Ruby on Rails, PostgreSQL, HTML, CSS, Bootstrap, Simple Form, Mapbox API, Flatpicker',
-    link: 'http://www.google.com/'
+    name: 'Spring Boot Library',
+    image: spring_boot_library,
+    skills: 'React, Typescript, Spring Boot, Spring MVC, MySQL,  HTML, CSS, Bootstrap',
+    link: 'http://www.google.com/',
+    description: 'Spring Boot Backend with MySQL scripts uploading books for Database. Front end with React and Typescript.'
   }
 ]
